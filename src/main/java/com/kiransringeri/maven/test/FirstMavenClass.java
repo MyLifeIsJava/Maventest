c9bff1d8110d001aa635c45598415e1cb939278c
kiransringeri <kiransringeri@gmail.com>
2018-08-13 13:25:58 +0530

    some changes done. Testing github username change.

--- src/main/java/com/kiransringeri/maven/test/FirstMavenClass.java
+++ src/main/java/com/kiransringeri/maven/test/FirstMavenClass.java
@@ -1,13 +1,108 @@
 package com.kiransringeri.maven.test;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class FirstMavenClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+  /**
+   * Method with some java8 features.
+   */
+  public void java8Features() {
+    String[] strArray = {"A", "B", "C", "D"};
+    List<String> list = Arrays.asList(strArray);
+    String result = list.stream().collect(Collectors.joining(","));
+    System.out.println(result);
+  }
+  /*
+   * // Method to test the FindBug plugin public void infiniteRecursiveLoop() {
+   * infiniteRecursiveLoop(); }
+   */
 
-	}
+  // Method to test PMD plugin -
+  /*
+   * public void emptyTryCatch() { try {
+   * 
+   * }catch(Throwable th) {
+   * 
+   * } }
+   */
+  /**
+   * The main method.
+   * 
+   * @param args input file
+   */
+  public static void main(String[] args) {
+    Scanner scanner = null;
+    try {
+      if (args.length > 1) {
+        System.err.println("Usage: " + FirstMavenClass.class.getSimpleName() + " [<input file>]");
+        System.exit(1);
+      }
+      if (args.length == 1) {
+        String fileName = args[0];
+        File file = new File(fileName);
+        if (!file.exists()) {
+          System.err.println("File \"" + fileName + "\" doesn't exist");
+          System.exit(1);
+        }
+        scanner = new Scanner(file, "UTF-8");
+      } else {
+        scanner = new Scanner(System.in, "UTF-8");
+      }
 
-	public String getString() {
-		return getClass().getSimpleName();
-	}
+      while (true) {
+        try {
+          String str = scanner.nextLine();
+
+          if (str == null || str.trim().length() == 0) { // Invalid input
+            continue;
+          }
+          String[] tokens = str.split("\\s");
+          String action = tokens[0];
+          if ("create_parking_lot".equals(action)) {
+            System.out.println("Created a parking lot with " + tokens[1]);
+          } else if ("park".equals(action)) {
+            System.out.println("Allocated " + tokens[1] + "," + tokens[2]);
+          } else if ("leave".equals(action)) {
+            System.out.println("De-allocated " + tokens[1]);
+          } else if ("registration_numbers_for_cars_with_colour".equals(action)) {
+            System.out.println("Reg Numbers for color " + tokens[1]);
+          } else if ("slot_numbers_for_cars_with_colour".equals(action)) {
+            System.out.println("Slot Numbers for color " + tokens[1]);
+          } else {
+            // Invalid action
+            System.out.println("Invalid input, ignoring this");
+            continue;
+          }
+
+        } catch (NoSuchElementException nse) {
+          break;
+        }
+      }
+    } catch (Throwable th) {
+      th.printStackTrace();
+    } finally {
+      if (scanner != null) {
+        scanner.close();
+      }
+    }
+  }
+
+  /**
+   * Divides the numbers.
+   * @param divident the dividend
+   * @param divisor the divisor
+   * @return
+   */
+  public int divideNumbers(int divident, int divisor) {
+    if (divisor == 0) 
+      throw new IllegalArgumentException("Divisor can't be zero");
+    
+    return divident / divisor;
+  }
 }
--- src/test/java/com/kiransringeri/maven/test/FirstMavenClassTest.java
+++ src/test/java/com/kiransringeri/maven/test/FirstMavenClassTest.java
@@ -1,15 +1,93 @@
 package com.kiransringeri.maven.test;
 
-import static org.junit.Assert.assertEquals;
+/**
+ * NOTES:
+ * 1. A test should have only 3 lines
+ * 		E.g: Given that I have an empty stack,
+ * 			 When I add an element to it,
+ * 			 The stack size will be 1.
+ *    We can push the complexities involved in each of these three lines to utility methods
+ *    
+ *    
+ */
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
-import org.junit.Test;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 public class FirstMavenClassTest {
-
+		
+	@DisplayName("Dummy Test")
 	@Test
-	public void testGetString() {
+	public void testJava8Features() {
 		FirstMavenClass obj = new FirstMavenClass();
-		assertEquals("Name do not match", "FirstMavenClass", obj.getString());
+		obj.java8Features();
+	}
+	
+	@DisplayName("Testing Main")
+	@Test
+	public void Should_Work_When_FileIsPssedToMainMethod() {
+		PrintStream stdOut = System.out;
+		File outFile = new File("check_output.txt");
+		try {
+			PrintStream ps = new PrintStream(new FileOutputStream(outFile)); 
+			System.setOut(ps);
+			FirstMavenClass.main(new String[] {"testing_data/input.txt"});
+			ps.flush();
+			ps.close();
+			System.setOut(stdOut);
+			
+			String expected = readFile("testing_data/output.txt");
+			String actual = readFile(outFile.getName());
+			assertEquals(expected, actual);
+		}catch(IOException th){
+			System.setOut(stdOut);
+			th.printStackTrace();
+			fail("Failed in main");
+		}finally {
+			outFile.delete();
+		}
+//		Test the output
+	}
+	
+	private String readFile(String file) throws IOException{
+		StringBuilder buffer = new StringBuilder();
+		String line = null;
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		while((line = br.readLine()) != null) {
+			buffer.append(line+"\n");
+		}
+		br.close();
+		return buffer.toString();
+	}
+//	Should_ExpectedBehavior_When_StateUnderTest
+	@DisplayName("Correct input")
+	@Test
+	public void Should_Work_When_CorrectInputIsGiven() {
+		FirstMavenClass obj = new FirstMavenClass();
+		assertEquals(3, obj.divideNumbers(18, 6));
 	}
 
+	@DisplayName("Divisor is ZERO")
+	@Test
+	public void Should_ThrowException_When_DivisorIsZERO() {
+		final FirstMavenClass obj = new FirstMavenClass();
+		Throwable exception = assertThrows(IllegalArgumentException.class, new Executable() {
+			public void execute() throws Throwable {
+				obj.divideNumbers(5, 0);
+			}
+		});
+        assertEquals("Divisor can't be zero", exception.getMessage());
+	}
+	
 }
